7adddcdb38e5768b0a59ca467b017f9d1a0325f2
gauri02saxena <gauri.saxena7379@gmail.com>
2023-07-27 21:54:52 +0530

    Encrypting using mongoose-encryption

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express= require("express");
 const ejs=require("ejs")
 const bodyParser= require("body-parser");
 const mongoose=require("mongoose");
+const encrypt=require("mongoose-encryption");
 
 const app=express();
 
@@ -12,10 +13,13 @@ app.use(express.static("public"));
 
 mongoose.connect("mongodb://127.0.0.1:27017/userDB",{useNewUrlParser:true, useUnifiedTopology:true});
 
-const userSchema= {
+const userSchema= new mongoose.Schema ({
     email: String,
     password: String
-}
+});
+
+const secret="Threemaykeepasecret";
+userSchema.plugin(encrypt, { secret: secret, encryptedFields: ['password'] });
 
 const User= mongoose.model("User", userSchema);
 
