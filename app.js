0e8bc352ba4d5800c3f08553fd1396c856dae436
gauri02saxena <gauri.saxena7379@gmail.com>
2023-07-27 19:33:21 +0530

    Added post routes

--- app.js
+++ app.js
@@ -35,7 +35,34 @@ app.get("/register", function(req,res)
     res.render("register");
 });
 
+app.post("/register", function(req,res)
+{
+    const newUser= new User({
+        email:req.body.username,
+        password:req.body.password
+    });
 
+    newUser.save()
+    .then(()=>{res.render("secrets")})
+    .catch((err)=>console.log(err));
+});
+
+app.post("/login", function(req,res)
+{
+    const username=req.body.username;
+    const password=req.body.password;
+
+    User.findOne({email:username})
+    .then((foundUser)=>{
+        if(foundUser.password===password)
+        {
+            res.render("secrets");
+        }
+    })
+    .catch((err)=>console.log(err));
+
+
+});
 
 app.listen(3000,function(){
     console.log("Server started on port 3000");
