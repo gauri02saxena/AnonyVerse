a63376cd3dba3026b8de6cb73d458fe990747982
gauri02saxena <gauri.saxena7379@gmail.com>
2023-07-27 23:15:36 +0530

    Encrypting using hashing

--- app.js
+++ app.js
@@ -3,9 +3,11 @@ const express= require("express");
 const ejs=require("ejs")
 const bodyParser= require("body-parser");
 const mongoose=require("mongoose");
-const encrypt=require("mongoose-encryption");
+const md5=require("md5");
+// const encrypt=require("mongoose-encryption");
 
 const app=express();
+
 
 app.use(bodyParser.urlencoded({extended:true}));
 app.set("view engine", "ejs");
@@ -20,7 +22,7 @@ const userSchema= new mongoose.Schema ({
 });
 
 
-userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ['password'] });
+// userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ['password'] });
 
 const User= mongoose.model("User", userSchema);
 
@@ -44,7 +46,7 @@ app.post("/register", function(req,res)
 {
     const newUser= new User({
         email:req.body.username,
-        password:req.body.password
+        password:md5(req.body.password)
     });
 
     newUser.save()
@@ -55,7 +57,7 @@ app.post("/register", function(req,res)
 app.post("/login", function(req,res)
 {
     const username=req.body.username;
-    const password=req.body.password;
+    const password=md5(req.body.password)
 
     User.findOne({email:username})
     .then((foundUser)=>{
